5b5bcbe06771187a0b7746d57a90fa5c9876aaea
ChristianMascolo <mascolokris@gmail.com>
2022-05-15 16:04:42 +0200

    1

--- eclipse-workspace/games_basement/src/main/java/db/Connection.java
+++ eclipse-workspace/games_basement/src/main/java/db/Connection.java
@@ -16,7 +16,7 @@ public class Connection {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.out.println("1");
+			System.out.println("11");
 		}  
 		return null;
 	}
