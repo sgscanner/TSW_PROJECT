7a6813ec8002751645af5f9c16619b4b6430a6d3
ChristianMascolo <mascolokris@gmail.com>
2022-05-15 17:00:20 +0200

    Merge branch 'main' of https://github.com/ChristianMascolo/TSW_PROJECT into main

--- eclipse-workspace/games_basement/src/main/java/db/Connection.java
+++ eclipse-workspace/games_basement/src/main/java/db/Connection.java
@@ -16,7 +16,7 @@ public class Connection {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.out.println("111");
+			System.out.println("1111");
 		}  
 		return null;
 	}
